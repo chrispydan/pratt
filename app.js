48fe39a4172a3a4454704f01669859ebbb3d4964
Chris Daniels <cdan198@gmail.com>
2017-09-21 17:30:47 -0700

    purifycss fix

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const Dato = require('spike-datocms')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
+const glob = require('glob')
+const PurifyCSSPlugin = require('purifycss-webpack')
+const path = require('path')
 const env = process.env.SPIKE_ENV
 
 const locals = {}
@@ -25,6 +28,10 @@ module.exports = {
       addDataTo: locals,
       token: '27b8085f8fdf8fe0015df7ad5ae88d',
       models: [{ name: 'project' }]
+    }),
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync(path.join(__dirname, 'views/*.html')),
     })
   ]
 }
